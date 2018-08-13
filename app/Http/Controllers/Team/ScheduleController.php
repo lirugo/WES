13e1abe21216b3aa41f2220851a5e5a81639edb5
lirguo <roostik21@gmail.com>
2018-08-13 16:38:46 +0300

    Fix saving events to schedule group

--- app/Http/Controllers/Team/ScheduleController.php
+++ app/Http/Controllers/Team/ScheduleController.php
@@ -8,6 +8,7 @@ use App\Schedule;
 use App\Team;
 use App\User;
 use Calendar;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Session;
@@ -55,22 +56,31 @@ class ScheduleController extends Controller
     }
 
     public function store(StoreSchedule $request, $name){
-        // Combine date and time
-        $start_date = date('Y-m-d H:i:s', strtotime("$request->start_date, $request->start_time"));
-        $end_date = date('Y-m-d H:i:s', strtotime("$request->end_date, $request->end_time"));
+        // Convert date
+        $start = Carbon::parse($request->start_date);
+        $end = Carbon::parse($request->end_date);
+        $start_date = Carbon::parse(date('Y-m-d H:i', strtotime("$request->start_date, $request->start_time")));
+        $end_date = Carbon::parse(date('Y-m-d H:i', strtotime("$request->start_date, $request->end_time")));
 
         // Find team
         $team = Team::where('name', $name)->first();
 
-        // Persist to db
-        Schedule::create([
-            'team_id' => $team->id,
-            'teacher_id' => $request->teacher_id,
-            'discipline_id' => $request->discipline_id,
-            'title' => $request->title,
-            'start_date' => $start_date,
-            'end_date' => $end_date,
-        ]);
+        while($start <= $end)
+        {
+            // Persist to db
+            Schedule::create([
+                'team_id' => $team->id,
+                'teacher_id' => $request->teacher_id,
+                'discipline_id' => $request->discipline_id,
+                'title' => $request->title,
+                'start_date' => $start_date,
+                'end_date' => $end_date,
+            ]);
+            // Add next day
+            $start = $start->addDay();
+            $start_date = $start_date->addDay();
+            $end_date = $end_date->addDay();
+        }
 
         // Flash msg
         Session::flash('success', 'Schedule was updated.');
