0eae4cee27c09a4148b174b5f1256475065bdae4
lirugo <roostik21@gmail.com>
2018-11-29 15:18:13 +0200

    Pretest - Show description of pretest

--- resources/views/team/pretest/pretests.blade.php
+++ resources/views/team/pretest/pretests.blade.php
@@ -3,7 +3,7 @@
     {{ Breadcrumbs::render('team-pretest-discipline', $team, $discipline) }}
 @endsection
 @section('content')
-    <div class="row">
+    <div class="row" id="pretest">
         @foreach($pretests as $pretest)
             <div class="col s12 m6">
                 <div class="card hoverable">
@@ -22,27 +22,66 @@
                     </div>
                     <div class="card-action right-align">
                         @role('student')
-                            @if($pretest->isAvailable(Auth::user()->id))
-                                <a href="{{url('/team/'.$team->name.'/pretest/discipline/'.$discipline->name.'/'.$pretest->id.'/pass')}}" class="indigo waves-effect waves-light btn-small right">Pass</a>
-                            @else
+                        @if($pretest->isAvailable(Auth::user()->id))
+                            <button @click="passPretest('{{$pretest->id}}')" class="indigo waves-effect waves-light btn-small right">Pass</button>
+                        @else
                             <a class="waves-effect waves-light btn btn-small right disabled"><i class="material-icons right">lock</i>locked</a>
-                            @endif
+                        @endif
                         @endrole
                         @role('teacher')
-                            <a href="{{url('/team/'.$team->name.'/pretest/discipline/'.$discipline->name.'/'.$pretest->id)}}" class="indigo waves-effect waves-light btn-small right">Open</a>
+                        <a href="{{url('/team/'.$team->name.'/pretest/discipline/'.$discipline->name.'/'.$pretest->id)}}" class="indigo waves-effect waves-light btn-small right">Open</a>
                         @endrole
                     </div>
                 </div>
             </div>
         @endforeach
+
+        @role('teacher')
+        {{--Floating button--}}
+        <div class="fixed-action-btn">
+            <a href="{{url('/team/'.$team->name.'/pretest/create')}}" class="btn-floating btn-large green tooltipped" data-position="left"
+               data-tooltip="Create Pretest">
+                <i class="large material-icons">add</i>
+            </a>
+        </div>
+        @endrole
     </div>
-    @role('teacher')
-    {{--Floating button--}}
-    <div class="fixed-action-btn">
-        <a href="{{url('/team/'.$team->name.'/pretest/create')}}" class="btn-floating btn-large green tooltipped" data-position="left"
-           data-tooltip="Create Pretest">
-            <i class="large material-icons">add</i>
-        </a>
+    <!-- Modal Structure -->
+    <div class="modal">
+        <div class="modal-content">
+            <h4>Attention</h4>
+            <p>You are about to be tested. You have one attempt </p>
+        </div>
+        <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-green btn-flat" onclick="redirect()">Open</a>
+        </div>
     </div>
-    @endrole
+@endsection
+
+@section('scripts')
+    <script>
+        // TODO::// remake this it a trash
+        var pretestId;
+        function redirect() {
+            window.location.href = '{{url('/team/'.$team->name.'/pretest/discipline/'.$discipline->name)}}/'  + pretestId + '/pass';
+        }
+
+        new Vue({
+            el: '#pretest',
+            data: {
+                elem: null,
+                instance: null,
+            },
+            created() {
+                this.elem = document.querySelector('.modal');
+                this.instance = M.Modal.init(this.elem);
+            },
+            methods: {
+                passPretest(id){
+                    this.instance.open();
+                    pretestId = id;
+                },
+            }
+        })
+    </script>
 @endsection
