1001ca4785cbf806c19ad0677df4c73b9a3da69d
lirugo <roostik21@gmail.com>
2019-01-22 15:16:55 +0200

    Moved create activity btn to next page

--- resources/views/team/activity/show.blade.php
+++ resources/views/team/activity/show.blade.php
@@ -47,11 +47,12 @@
                 </div>
             </div>
         @endforeach
+
+        @role('teacher')
+        {{--Link create page--}}
+        <div class="fixed-action-btn">
+            <a class="btn-floating btn-large waves-effect waves-light green" href="{{url('team/'.$team->name.'/activity/create')}}"><i class="material-icons">add</i></a>
+        </div>
+        @endrole
     </div>
-@endsection
-
-@section('scripts')
-    <script>
-
-    </script>
 @endsection
