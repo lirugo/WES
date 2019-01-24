7b9b2978a38e3105c43b209171e1c22df0ddf0ff
lirugo <roostik21@gmail.com>
2019-01-24 15:41:38 +0200

    Library file name Fix#4

--- app/Http/Controllers/Library/LibraryController.php
+++ app/Http/Controllers/Library/LibraryController.php
@@ -62,7 +62,7 @@ class LibraryController extends Controller
         $library->year = $request->year;
         $library->image = $request->avatar;
         $filePath = Storage::disk('library')->put('/', $request->file);
-        $library->file =  basename($filePath).pathinfo($fileName, PATHINFO_EXTENSION);
+        $library->file =  basename($filePath);
         $library->save();
 
         foreach (json_decode($request->tags) as $tag){
