3a4a1604a16893533e32960d4eeab6f354076855
lirugo <roostik21@gmail.com>
2019-03-13 17:23:38 +0200

    Added chat icon in menu

--- resources/views/_includes/nav/left.blade.php
+++ resources/views/_includes/nav/left.blade.php
@@ -14,6 +14,7 @@
         <li><a href="{{url('/manage')}}"><i class="material-icons">apps</i>Main</a></li>
         <li><a href="{{url('/news')}}"><i class="material-icons">new_releases</i>News</a></li>
         <li><a href="{{url('/notification')}}"><i class="material-icons">notifications</i>Notifications</a></li>
+        <li><a href="#"><i class="material-icons">chat</i>Chat</a></li>
         <li><a href="{{url('/library')}}"><i class="material-icons">library_books</i>Library</a></li>
         <li><a href="{{url('/changelog')}}"><i class="material-icons">turned_in</i>Change Log</a></li>
 
