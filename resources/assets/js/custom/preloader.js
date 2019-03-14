cfead9a37a85f6836eec1e5e6c6a3c69b3893086
lirugo <roostik21@gmail.com>
2019-03-14 13:00:45 +0200

    Preloader disabled

--- resources/assets/js/custom/preloader.js
+++ resources/assets/js/custom/preloader.js
@@ -1,5 +1,5 @@
-document.body.onload = function () {
-    var preloader = document.getElementById('page-preloader');
-    if(!preloader.classList.contains('done'))
-        preloader.classList.add('done');
-};
+// document.body.onload = function () {
+//     var preloader = document.getElementById('page-preloader');
+//     if(!preloader.classList.contains('done'))
+//         preloader.classList.add('done');
+// };
