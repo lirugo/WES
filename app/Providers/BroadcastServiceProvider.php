8962fc00b2c4903be300da54e774059782196e07
lirugo <roostik21@gmail.com>
2019-03-16 16:58:45 +0200

    Fix chat

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Chat\Session;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Broadcast;
 
@@ -16,6 +17,23 @@ class BroadcastServiceProvider extends ServiceProvider
     {
         Broadcast::routes();
 
-        require base_path('routes/channels.php');
+//        require base_path('routes/channels.php');
+
+        Broadcast::channel('App.User.{id}', function ($user, $id) {
+            return (int) $user->id === (int) $id;
+        });
+
+        Broadcast::channel('Chat', function ($user) {
+            return $user;
+        });
+
+        Broadcast::channel('Chat.{session}', function ($user, Session $session) {
+            if($user->id == $session->user1_id || $user->id == $session->user2_id)
+                return true;
+            else
+                return false;
+        });
+
     }
+
 }
--- routes/channels.php
+++ routes/channels.php
@@ -14,17 +14,3 @@
 use App\Models\Chat\Session;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
-
-Broadcast::channel('Chat', function ($user) {
-    return $user;
-});
-
-Broadcast::channel('Chat.{session}', function ($user, Session $session) {
-    if($user->id == $session->user1_id || $user->id == $session->user2_id)
-        return true;
-    else
-        return false;
-});
