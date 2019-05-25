60dffae42708d03948756b09c149230e0d375c48
lirugo <roostik21@gmail.com>
2019-05-25 10:02:12 +0300

    Education Materials #1 - minor style, marked as old

--- resources/views/team/material/show.blade.php
+++ resources/views/team/material/show.blade.php
@@ -3,26 +3,33 @@
     {{ Breadcrumbs::render('team-material-show', $team) }}
 @endsection
 @section('content')
+    {{--    OLD EDUCATION MATERIALS--}}
     <div class="row">
-        <div class="col s12 m6">
-            @foreach($materials as $material)
-                <div class="card-panel">
-                    {!! Form::open(['url' => '/team/material/getFile/'.$material->file, 'method' => 'POST']) !!}
-                    <button type="submit" class="btn btn-small indigo waves-effect">
-                        <i class="material-icons left">cloud_download</i>
-                        <span class="m-l-5">{{$material->name}}</span>
-                    </button>
-                    {!! Form::close() !!}
-                    <p><blockquote>Uploaded - {{$material->created_at->diffForHumans()}}</blockquote></p>
-                    @if(Auth::user()->hasRole('teacher') || Auth::user()->hasRole('manager'))
-                        {!! Form::open(['route' => ['team.material.delete', $material->id]]) !!}
-                            <button type="submit" class="waves-effect waves-light btn btn-small red right"><i class="material-icons">delete</i></button>
-                        {!! Form::close() !!}
-                    @endif
+        <div class="col s12">
+            <div class="card-panel">
+                <div class="row m-b-0 m-t-0">
+                    @foreach($materials as $material)
+                        <div class="col s12 m6 m-b-5 m-t-5">
+                            @if(Auth::user()->hasRole('teacher') || Auth::user()->hasRole('manager'))
+                                {!! Form::open(['route' => ['team.material.delete', $material->id]]) !!}
+                                <button type="submit" class="waves-effect waves-light btn btn-small red left m-r-5"><i class="material-icons">delete</i></button>
+                                {!! Form::close() !!}
+                            @endif
+                            {!! Form::open(['url' => '/team/material/getFile/'.$material->file, 'method' => 'POST']) !!}
+                            <button type="submit" class="btn btn-small indigo waves-effect p-b-5">
+                                <i class="material-icons left">cloud_download</i>
+                                <span class="m-l-5">{{$material->name}}</span>
+                            </button>
+                            <small>{{$material->created_at->diffForHumans()}}</small>
+                            {!! Form::close() !!}
+                        </div>
+                    @endforeach
                 </div>
-            @endforeach
+            </div>
         </div>
-        @if(Auth::user()->hasRole(['manager', 'teacher']))
+    </div>
+
+    @if(Auth::user()->hasRole(['manager', 'teacher']))
         {{--Floating button--}}
         <div class="fixed-action-btn">
             <a href="{{url('/team/'.$team->name.'/material/create')}}" class="btn-floating btn-large green tooltipped" data-position="left"
@@ -30,6 +37,5 @@
                 <i class="large material-icons">add</i>
             </a>
         </div>
-        @endif
-    </div>
+    @endif
 @endsection
