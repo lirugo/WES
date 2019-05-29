ad263f4c705441e09a6e67483447e72f2d6bcde6
lirugo <roostik21@gmail.com>
2019-05-29 17:32:35 +0300

    Minor

--- resources/views/team/material/show.blade.php
+++ resources/views/team/material/show.blade.php
@@ -5,30 +5,32 @@
 
 @section('content')
     {{--    OLD EDUCATION MATERIALS--}}
-    <div class="row m-b-0 m-t-0">
-        <div class="col s12">
-            <div class="card-panel">
-                <div class="row m-b-0 m-t-0">
-                    @foreach($materials as $material)
-                        <div class="col s12 m6 m-b-5 m-t-5">
-                            {{--                            @if(Auth::user()->hasRole('teacher') || Auth::user()->hasRole('manager'))--}}
-                            {{--                                {!! Form::open(['route' => ['team.material.delete', $material->id]]) !!}--}}
-                            {{--                                <button type="submit" class="waves-effect waves-light btn btn-small red left m-r-5"><i class="material-icons">delete</i></button>--}}
-                            {{--                                {!! Form::close() !!}--}}
-                            {{--                            @endif--}}
-                            {!! Form::open(['url' => '/team/material/getFile/'.$material->file, 'method' => 'POST']) !!}
-                            <button type="submit" class="btn btn-small indigo waves-effect p-b-5">
-                                <i class="material-icons left">cloud_download</i>
-                                <span class="m-l-5">{{$material->name}}</span>
-                            </button>
-                            {{--                            <small>{{$material->created_at->diffForHumans()}}</small>--}}
-                            {!! Form::close() !!}
-                        </div>
-                    @endforeach
+    @if(count($materials) > 0)
+        <div class="row m-b-0 m-t-0">
+            <div class="col s12">
+                <div class="card-panel">
+                    <div class="row m-b-0 m-t-0">
+                        @foreach($materials as $material)
+                            <div class="col s12 m6 m-b-5 m-t-5">
+                                {{--                            @if(Auth::user()->hasRole('teacher') || Auth::user()->hasRole('manager'))--}}
+                                {{--                                {!! Form::open(['route' => ['team.material.delete', $material->id]]) !!}--}}
+                                {{--                                <button type="submit" class="waves-effect waves-light btn btn-small red left m-r-5"><i class="material-icons">delete</i></button>--}}
+                                {{--                                {!! Form::close() !!}--}}
+                                {{--                            @endif--}}
+                                {!! Form::open(['url' => '/team/material/getFile/'.$material->file, 'method' => 'POST']) !!}
+                                <button type="submit" class="btn btn-small indigo waves-effect p-b-5">
+                                    <i class="material-icons left">cloud_download</i>
+                                    <span class="m-l-5">{{$material->name}}</span>
+                                </button>
+                                {{--                            <small>{{$material->created_at->diffForHumans()}}</small>--}}
+                                {!! Form::close() !!}
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endif
 
     {{--    Show categories--}}
     <div class="row">
@@ -94,7 +96,7 @@
         </div>
 
         <div class="col s12 m4">
-        {{--    Show links--}}
+            {{--    Show links--}}
             <ul class="collection with-header">
                 <li class="collection-header"><h6 class="center">Links</h6></li>
                 @foreach($links as $link)
@@ -115,7 +117,6 @@
             {{-- Show Video--}}
             <div class="card-panel p-t-10">
                 <h6 class="center">Video</h6>
-                <hr>
                 @foreach($videos as $video)
                     @role(['teacher', 'manager'])
                     {!! Form::open(['route' => ['team.video.delete', $video->id]]) !!}
