ade351b6baacfbf4bbcec98da412b9a8f536b2f3
lirugo <roostik21@gmail.com>
2019-07-19 12:19:49 +0300

    Close chat for users

--- app/Http/Controllers/Chat/ChatController.php
+++ app/Http/Controllers/Chat/ChatController.php
@@ -4,24 +4,31 @@ namespace App\Http\Controllers\Chat;
 
 use App\Http\Resources\UserResource;
 use App\Models\Conversation\Conversation;
-use Illuminate\Http\Request;
 use Auth;
+use Session;
 use App\Http\Controllers\Controller;
 
 class ChatController extends Controller
 {
     public function index(){
-        return view('chat.index');
+        //Hide chat page
+        Session::flash('warning', 'Chat closed on development');
+        return back();
+//        return view('chat.index');
     }
 
     public function conversations(){
-        $team = Auth::user()->teams()->first();
-        if($team)
-            $users = UserResource::collection($team->getMembers());
-        else
-            $users = null;
-        return view('chat.conversations')
-            ->withUsers($users);
+        //Hide chat page
+        Session::flash('warning', 'Chat closed on development');
+        return back();
+
+//        $team = Auth::user()->teams()->first();
+//        if($team)
+//            $users = UserResource::collection($team->getMembers());
+//        else
+//            $users = null;
+//        return view('chat.conversations')
+//            ->withUsers($users);
     }
 
     public function show(Conversation $conversation){
