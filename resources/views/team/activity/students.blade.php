a3c6b146727cffe1fc9b82471f79e150a0905637
lirugo <roostik21@gmail.com>
2019-11-04 12:44:43 +0200

    Group work - show mark in list of students

--- resources/views/team/activity/students.blade.php
+++ resources/views/team/activity/students.blade.php
@@ -8,8 +8,14 @@
         @foreach($team->getStudents() as $student)
             <div class="col s12 m4">
                 <div class="card hoverable">
+                    @if($activity->getMark($student->id))
+                        <span data-badge-caption="" class="new badge grey right">Mark {{$activity->getMark($student->id) ? $activity->getMark($student->id)->mark : ''}}</span>
+                    @else
+                        <span data-badge-caption="" class="new badge orange right">No mark</span>
+                    @endif
                     <div class="card-content">
                         <span class="card-title center-align">{{$student->getShortName()}}</span>
+
                     </div>
                     <div class="card-action">
                         <a href="{{url('team/'.$team->name.'/activity/'.$discipline->name.'/pass/'.$activity->id.'/'.$student->id)}}" class=" waves-effect waves-light btn-small right indigo">@lang('app.Open')</a>
