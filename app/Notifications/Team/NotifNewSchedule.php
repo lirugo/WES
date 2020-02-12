4797c54a0e030eed819d6cfc4ca61c820416edab
lirugo <roostik21@gmail.com>
2020-02-12 14:57:10 +0200

    Disable schedule notification

--- app/Notifications/Team/NotifNewSchedule.php
+++ app/Notifications/Team/NotifNewSchedule.php
@@ -57,10 +57,10 @@ class NotifNewSchedule extends Notification
      */
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-                    ->line('Your schedule was updated')
-                    ->action('Open page', url('/team/'.$this->schedule->team->name.'/schedule'))
-                    ->line('Thank you for using our application!');
+//        return (new MailMessage)
+//                    ->line('Your schedule was updated')
+//                    ->action('Open page', url('/team/'.$this->schedule->team->name.'/schedule'))
+//                    ->line('Thank you for using our application!');
     }
 
     /**
@@ -71,10 +71,10 @@ class NotifNewSchedule extends Notification
      */
     public function toArray($notifiable)
     {
-        return [
-            'title' => 'Your schedule was be updated',
-            'body' => $this->schedule->title.' '.$this->schedule->start_date.' - '.$this->schedule->end_date,
-            'url' => url('/team/'.$this->schedule->team->name.'/schedule'),
-        ];
+//        return [
+//            'title' => 'Your schedule was be updated',
+//            'body' => $this->schedule->title.' '.$this->schedule->start_date.' - '.$this->schedule->end_date,
+//            'url' => url('/team/'.$this->schedule->team->name.'/schedule'),
+//        ];
     }
 }
