ebcddcc5e914c85b110407c05a3b2dfd557a7e91
roostik21@gmail.com <roostik21@gmail.com>
2020-10-15 17:41:12 +0300

    Hide disabled courses in a list

--- resources/views/team/courses/disciplines.blade.php
+++ resources/views/team/courses/disciplines.blade.php
@@ -6,16 +6,19 @@
     {{--Show type activity--}}
     <div class="row">
         @foreach($disciplines as $discipline)
-            <div class="col s12 m6">
-                <div class="card hoverable">
-                    <div class="card-content">
-                        <span class="card-title center-align">{{$discipline->getDiscipline->display_name}}</span>
-                    </div>
-                    <div class="card-action">
-                        <a href="{{url('team/'.$team->name.'/courses/'.$discipline->getDiscipline->name)}}" class=" waves-effect waves-light btn-small right indigo">@lang('app.Open')</a>
+            @if(!$discipline->disabled)
+                <div class="col s12 m6">
+                    <div class="card hoverable">
+                        <div class="card-content">
+                            <span class="card-title center-align">{{$discipline->getDiscipline->display_name}}</span>
+                        </div>
+                        <div class="card-action">
+                            <a href="{{url('team/'.$team->name.'/courses/'.$discipline->getDiscipline->name)}}"
+                               class=" waves-effect waves-light btn-small right indigo">@lang('app.Open')</a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endif
         @endforeach
     </div>
 @endsection
