bf4f8a464877e102fed639ce282bd5d0584b89bb
Rostyslav Shynlov <rshynlov@gen2dev.com>
2023-09-10 13:01:56 +0300

    Minor left menu changes for manager

--- resources/views/_includes/nav/left.blade.php
+++ resources/views/_includes/nav/left.blade.php
@@ -1,48 +1,52 @@
 @auth
-    <ul id="slide-out" class="sidenav sidenav-fixed" >
-        {{-- Header --}}
-        <li><div class="user-view">
-                <div class="background indigo darken-2">
-                </div>
-                <a href="#user"><img class="circle" src="{{asset('/uploads/avatars/'.Auth::user()->avatar)}}"></a>
-                <a href="#name"><span class="white-text name">{{Auth::user()->getShortName()}}</span></a>
+<ul id="slide-out" class="sidenav sidenav-fixed">
+  {{--Header--}}
+  <li>
+    <div class="user-view">
+      <div class="background indigo darken-2">
+      </div>
+      <a href="#user"><img class="circle" src="{{asset('/uploads/avatars/'.Auth::user()->avatar)}}"></a>
+      <a href="#name"><span class="white-text name">{{Auth::user()->getShortName()}}</span></a>
 
-                <a href="#email"><span class="white-text email">{{Auth::user()->email}}</span></a>
-            </div>
-        </li>
-        <li><a href="{{url('/manage')}}"><i class="material-icons">apps</i>@lang('app.Main')</a></li>
-        <li><a href="{{url('/news')}}"><i class="material-icons">new_releases</i>@lang('app.News')</a></li>
-        <li>
-            <a href="{{url('/notification')}}">
-                <i class="material-icons">notifications</i>
-                @lang('app.Notifications')
-                @if(count(Auth::user()->unreadNotifications) > 0)
-                    <span class="badge new red">{{count(Auth::user()->unreadNotifications)}}</span>
-                @endif
-            </a>
-        </li>
-        <li><a href="{{url('/conversations')}}"><i class="material-icons">chat</i>@lang('app.Chat')</a></li>
-        <li><a href="{{url('/library')}}"><i class="material-icons">library_books</i>@lang('app.Library')</a></li>
+      <a href="#email"><span class="white-text email">{{Auth::user()->email}}</span></a>
+    </div>
+  </li>
+  <li><a href="{{url('/manage')}}"><i class="material-icons">apps</i>@lang('app.Main')</a></li>
+  <li><a href="{{url('/news')}}"><i class="material-icons">new_releases</i>@lang('app.News')</a></li>
+  <li>
+    <a href="{{url('/notification')}}">
+      <i class="material-icons">notifications</i>
+      @lang('app.Notifications')
+      @if(count(Auth::user()->unreadNotifications) > 0)
+      <span class="badge new red">{{count(Auth::user()->unreadNotifications)}}</span>
+      @endif
+    </a>
+  </li>
+  <li><a href="{{url('/conversations')}}"><i class="material-icons">chat</i>@lang('app.Chat')</a></li>
+  <li><a href="{{url('/library')}}"><i class="material-icons">library_books</i>@lang('app.Library')</a></li>
 
-        {{-- Administrator --}}
-        @include('_includes.nav.roles.administrator')
+  {{--Administrator--}}
+  @include('_includes.nav.roles.administrator')
 
-        {{-- Top-Manager --}}
-        @include('_includes.nav.roles.top-manager')
+  {{--Top - Manager--}}
+  @include('_includes.nav.roles.top-manager')
 
-        {{-- Manager --}}
-        @include('_includes.nav.roles.manager')
+  {{--Manager--}}
+  @include('_includes.nav.roles.manager')
 
-        {{-- Teacher --}}
-        @include('_includes.nav.roles.teacher')
+  {{--Teacher--}}
+  @include('_includes.nav.roles.teacher')
 
-        {{-- Student --}}
-        @include('_includes.nav.roles.student')
+  {{--Student--}}
+  @include('_includes.nav.roles.student')
 
-        {{-- Settings --}}
-        <li><div class="divider"></div></li>
-        <li><a class="subheader">@lang('app.Settings')</a></li>
-        <li><a href="{{url('/user/settings')}}"><i class="material-icons">settings</i>@lang('app.Settings')</a></li>
-        <li><a href="{{url('/user/profile')}}" style="margin-bottom: 5em"><i class="material-icons">assignment_ind</i>@lang('app.Profile')</a></li>
-    </ul>
+  {{--Settings--}}
+  <li>
+    <div class="divider"></div>
+  </li>
+  <li><a class="subheader">@lang('app.Settings')</a></li>
+  <li><a href="{{url('/user/settings')}}"><i class="material-icons">settings</i>@lang('app.Settings')</a></li>
+  <li><a href="{{url('/user/profile')}}" style="margin-bottom: 5em"><i class="material-icons">assignment_ind</i>@lang('app.Profile')</a>
+  </li>
+</ul>
 @endauth
--- resources/views/_includes/nav/roles/manager.blade.php
+++ resources/views/_includes/nav/roles/manager.blade.php
@@ -1,26 +1,38 @@
 @if(Auth::user()->hasRole('manager'))
-    <li><div class="divider"></div></li>
-    <li><a class="subheader">@lang('app.Manager')</a></li>
+<li>
+  <div class="divider"></div>
+</li>
+<li><a class="subheader">@lang('app.Manager')</a></li>
+<li>
+  <ul class="collapsible">
     <li>
-        <ul class="collapsible">
-            <li>
-                <div class="collapsible-header"><i class="material-icons">person</i>@lang('app.Students')</div>
-                <div class="collapsible-body">
-                    <ul>
-                        <li><a href="{{url('/student/create')}}"><i class="material-icons">person_add</i>@lang('app.Create a new student')</a></li>
-                        <li><a href="{{url('/student/')}}"><i class="material-icons">group</i>@lang('app.Show all students')</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li>
-                <div class="collapsible-header"><i class="material-icons">group</i>@lang('app.Groups')</div>
-                <div class="collapsible-body">
-                    <ul>
-                        <li><a href="{{url('/team/create')}}"><i class="material-icons">group_add</i>@lang('app.Create a new group')</a></li>
-                        <li><a href="{{url('/team')}}"><i class="material-icons">group</i>@lang('app.Show all groups')</a></li>
-                    </ul>
-                </div>
-            </li>
+      <div class="collapsible-header"><i class="material-icons">person</i>@lang('app.Students')</div>
+      <div class="collapsible-body">
+        <ul>
+          <li><a href="{{url('/student/create')}}"><i class="material-icons">person_add</i>
+              @lang('app.Create a new student')</a></li>
+          <li><a href="{{url('/student/')}}"><i class="material-icons">group</i>@lang('app.Show all students')</a></li>
         </ul>
+      </div>
     </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">group</i>@lang('app.Groups')</div>
+      <div class="collapsible-body">
+        <ul>
+          <li><a href="{{url('/team/create')}}"><i class="material-icons">group_add</i>
+              @lang('app.Create a new group')</a></li>
+          <li><a href="{{url('/team')}}"><i class="material-icons">group</i>@lang('app.Show all groups')</a></li>
+
+          <li>
+            <div class="divider"></div>
+          </li>
+          @foreach(Auth::user()->teams() as $team)
+          <li><a href="{{url('/team/'.$team->name)}}"><i class="material-icons">group</i>{{$team->display_name}}</a>
+          </li>
+          @endforeach
+        </ul>
+      </div>
+    </li>
+  </ul>
+</li>
 @endif
